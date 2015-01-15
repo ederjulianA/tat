843698b7f60f5d05998508ae15da141b6e0cb6ad
carlos <cvcarlosandres39@gmail.com>
2015-01-15 13:30:10 -0500

    Revert "barrios"
    This reverts commit 3a18783692c970b6d02146141d7c21d24b5c086c.

--- app/views/tiendo/admin/barrios.blade.php
+++ app/views/tiendo/admin/barrios.blade.php
@@ -39,51 +39,41 @@
              <div class="panel panel-smart">
           <div class="panel-heading">
                 <h3 class="panel-title">
-                  Asignar dias de visita a los barrios
+                  Asignar dias de visita a os barrios
                 </h3>
           </div>
-          <div class="panel-body"> 
-          <table class="table table-responsive">
-                <tr>
-                  <th><h2>Barrios</h2></th>
-                  <th><h2>Días</h2></th>
-                  <th><h2>Agendar</h2></th>
-                  <th><h2>Días Agendados</h2></th>
-                  <th><h2>Eliminar día agendado</h2></th>
-                </tr>
-                @foreach($barrios as $barrio)
-                <tr>
-                  <td><h3>{{$barrio->bar_nom}}</h3></td>
-                
-            <!--<div class="barrioItm"> 
-                <h2>{{$barrio->bar_nom}} -->
-
+          <div class="panel-body">
+          @foreach($barrios as $barrio)
+            <div class="barrioItm">
+                <h2>{{$barrio->bar_nom}} 
                   <form method="post" action="{{URL::route('addDia')}}">
-                  <td>{{ Form::select('ndia', $dias,['class'=>'Ndia']) }}</td>                  
-                  <td><input type="hidden" name="barrio_id" value="{{$barrio->id}}">
-                  <input type="submit" class="btn btn-success" value="Agregar Dia"></td>
+                  {{ Form::select('ndia', $dias,['class'=>'Ndia']) }}
+                    <input type="hidden" name="barrio_id" value="{{$barrio->id}}">
+                    <input type="submit" class="btn btn-success" value="Agregar Dia">
                   </form>
-    
-                    <div class="diaV">
-                  
-                      @foreach($barrio->dias as $dia)
+                </h2>
+                <div class="diaV">
+                  <ul>
+                    @foreach($barrio->dias as $dia)
                      
-                      <td><h3>{{$dia->dia_nom}}</h3></td> 
-
-                       <form method="post" action="{{URL::route('deleteDay')}}">
+                      <li>  {{$dia->dia_nom}}---
+                        <form method="post" action="{{URL::route('deleteDay')}}">
                           <input type="hidden" name="dia_id" value="{{$dia->id}}">
                           <input type="hidden" name="barrio_id" value="{{$barrio->id}}">
-                          <td><input type="submit" class="btn btn-danger" value="Eliminar Dia"></td>
-                       </form>
+                          <input type="submit" class="btn btn-danger" value="Eliminar Dia">
+                        </form>
                       </li>
-                      @endforeach
-                    </tr>            
-                    </div>
-                  @endforeach  
-              </table>
-                                
+                      
+                    @endforeach 
+                 </ul>
+                  
+                </div>
+                  
+                
             </div>
-           
+          @endforeach 
+            
+
           </div>
         </div>
           
