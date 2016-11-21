4ecbbb95bec12842c159e9b5f8a226cc2af4ce1a
ederjulianA <ederalvarez2091057@gmail.com>
2016-11-21 17:57:33 -0500

    Printer

--- app/routes.php
+++ app/routes.php
@@ -370,7 +370,28 @@ Route::group(['before' => 'auth'], function() {
 			$query-> where('price','<=',$max_price);
 		}
 	})->get();
-	return View::make('index', compact(['products']));*/
+	return View::make('index', compact(['products']));
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <script type="text/javascript">
+            function imprimir() {
+                if (window.print) {
+                    window.print();
+                } else {
+                    alert("La función de impresion no esta soportada por su navegador.");
+                }
+            }
+        </script>
+    </head>
+    <body onload="imprimir();">
+        Hola PHPCentral!
+    </body>
+</html>
+	*/
 
 
 
